1e848c69e80c7d8a55f8fae6f370facb0dffdf41
thehaigo <sirannha@gmail.com>
2021-11-14 01:43:31 +0900

    uuid add

--- mix.exs
+++ mix.exs
@@ -45,7 +45,8 @@ defmodule LiveYolo.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:uuid, "~> 1.1"}
     ]
   end
 
